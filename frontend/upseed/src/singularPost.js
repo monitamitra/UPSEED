d61a024bbf12ca52d3b3e7675696b7cf953ca167
Stephanie Nguyen <S.nguyen6734@gmail.com>
2024-03-23 06:16:07 -0500

    got rid of error

--- frontend/upseed/src/singularPost.js
+++ frontend/upseed/src/singularPost.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Feed() {
   // Assuming posts is an array of post objects
-  const posts = [...];
+//   const posts = [...];
 
   return (
     <div>
